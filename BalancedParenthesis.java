f8d1f9d919294a784484eea9d0c0474ecafc62e5
Himani21Joshi <94060486+Himani21Joshi@users.noreply.github.com>
2022-01-31 06:20:20 -0800

    first commit

---
+++ BalancedParenthesis.java
@@ -0,0 +1,67 @@
+package com.greatlearning.lab3;
+
+import java.util.Stack;
+
+public class BalancedParenthesis {
+
+	public static void main(String[] args) {
+		
+		String input = "([[{}]])";
+		
+		boolean result = isParenthesisBalanced(input);
+		
+		if(result) 
+		{
+			System.out.println("The entered String has Balanced Brackets");		
+		}
+		else 
+		{
+			System.out.println("The entered String do not contain Balanced Brackets");
+		}
+	}
+
+	private static boolean isParenthesisBalanced(String input) 
+	{
+		
+		Stack<Character> stack = new Stack<Character>();
+		char[] inputArray = input.toCharArray();
+		for(int i = 0; i < inputArray.length; i++) 
+		{
+			if(inputArray[i] == '(' || inputArray[i] == '[' || inputArray[i] == '{') {
+				stack.push(inputArray[i]);
+				continue;
+			}
+			if(stack.isEmpty()) {
+				return false;
+			}
+			char c;
+			switch(inputArray[i]) {
+			
+			case ')':
+				c = stack.pop();
+				if(c == ']' || c == '}') {
+					return false;
+				}
+				break;
+			case ']':
+				c = stack.pop();
+				if(c == ')' || c == '}') {
+					return false;
+				}
+				break;
+			case '}':
+			    c = stack.pop();
+				if(c == ']' || c == ')') {
+					return false;
+				}
+				break;
+			}
+		}
+		if(stack.isEmpty()) {
+			return true;
+		}
+		else {
+		    return false;
+		}
+	}
+}
